298bee583bee0e7688fa87cb2eadac7e377332da
aki <aki@mymail.com>
2023-08-19 12:38:38 +0900

    n列のカンマ区切りの整数もパースできるようになった

--- getting_started/src/ss_4/csv_parser.rs
+++ getting_started/src/ss_4/csv_parser.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
-use nom::{IResult, bytes::complete::tag};
+use nom::{IResult, bytes::complete::tag, branch::alt};
 use nom::sequence::{separated_pair, tuple};
-use nom::character::complete::{i32,alpha0};
+use nom::character::complete::i32;
 
 #[derive(Debug,PartialEq)]
 pub struct Point3D {
@@ -42,6 +42,26 @@ fn parse_to_point_3d(input: &str) -> IResult<&str, Point3D> {
     Ok((remaining, Point3D{x,y,z}))
 }
 
+fn first_integer(input: &str) -> IResult<&str, (i32, &str)> {
+    tuple((
+            i32,
+            alt((tag(","),tag("")))
+    ))(input)
+}
+
+fn parse_integer_nd(input: &str, result: &mut Vec<i32>){
+    match first_integer(input) {
+        Err(e) => panic!("Error: {}",e),
+        Ok((remain,(value, _delimiter))) => {
+            result.push(value);
+            if remain == "" {
+                return;
+            }
+            parse_integer_nd(remain, result);
+        }
+    }
+}
+
 mod tests{
     use super::*;
 
@@ -69,6 +89,39 @@ mod tests{
 
         Ok(())
     }
+
+    #[test]
+    fn test_first_integer() -> Result<(), Box<dyn Error>> {
+        let (remain, (value, _delimiter)) = first_integer("1,2,3")?;
+        assert_eq!((1,"2,3"), (value, remain));
+
+        let (remain, (value, _delimiter)) = first_integer("1,2,3,")?;
+        assert_eq!((1,"2,3,"), (value, remain));
+
+        let (remain, (value, _delimiter)) = first_integer("3,")?;
+        assert_eq!((3,""), (value, remain));
+
+        let (_remain, (_value, _delimiter)) = first_integer("3")?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_integer_nd() -> Result<(), Box<dyn Error>> {
+        let mut result = vec![];
+        parse_integer_nd("1,2,3,", &mut result);
+        assert_eq!(vec![1,2,3], result);
+
+        let mut result = vec![];
+        parse_integer_nd("1,2,3,4,5,", &mut result);
+        assert_eq!(vec![1,2,3,4,5], result);
+
+        let mut result = vec![];
+        parse_integer_nd("1,2,3", &mut result);
+        assert_eq!(vec![1,2,3], result);
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -80,6 +133,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let (_remaining, point) = parse_to_point_3d("1,2,3")?;
     println!("point = {:?}", point);
+
+    let mut result_vector = vec![];
+    parse_integer_nd("1,2,3,4", &mut result_vector);
+    println!("result_vector = {:?}", result_vector);
 
     Ok(())
 }
