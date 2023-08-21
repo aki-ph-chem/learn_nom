0050924a82d0a216afe7077dfa5292bfc5346d6c
aki <aki@mymail.com>
2023-08-21 16:44:18 +0900

    takeの例を追加

--- getting_started/src/ss_5/take_.rs
+++ getting_started/src/ss_5/take_.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use nom::bytes::complete::{take_until, take_till, take_while};
+use nom::bytes::complete::{take_until, take_till, take_while, take};
 use nom::character::is_alphabetic;
 use nom::IResult;
 
@@ -13,6 +13,10 @@ fn until_end(input: &str) -> IResult<&str, &str> {
 
 fn while_alphabet(input: &[u8]) -> IResult<&[u8], &[u8]> {
     take_while(is_alphabetic)(input)
+}
+
+fn take_nth(input: &str, count: usize) -> IResult<&str,&str> {
+    take(count)(input)
 }
 
 #[cfg(test)]
@@ -36,6 +40,12 @@ mod tests {
         assert_eq!(while_alphabet(b"12345"), Ok((&b"12345"[..], &b""[..])));
         assert_eq!(while_alphabet(b"abcxyz"), Ok((&b""[..], &b"abcxyz"[..])));
     }
+
+    #[test]
+    fn test_take_nth() {
+        assert_eq!(take_nth("123456",3), Ok(("456", "123")));
+        assert_eq!(take_nth("abc_def",4), Ok(("def", "abc_")));
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -43,6 +53,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!(" remaining = {}\n result = {}", remaining, result);
 
     let (remaining, result) = until_end("def fun(x,y) x + y end")?;
+    println!(" remaining = {}\n result = {}", remaining, result);
+
+    let (remaining, result) = while_alphabet(b"abc_123")?;
+    println!(" remaining = {}\n result = {}", 
+        std::str::from_utf8(remaining).unwrap(), std::str::from_utf8(result).unwrap());
+
+    let (remaining, result) = take_nth("123456", 3)?;
     println!(" remaining = {}\n result = {}", remaining, result);
 
     Ok(())
